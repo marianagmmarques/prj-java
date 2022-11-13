12959a2daa1ffff157155d7cc6855563310c0ab9
gurgelrga <71472948+gurgelrga@users.noreply.github.com>
2022-11-13 15:40:17 -0300

    Arrumando

--- src/main/java/org/senai/prjjava/entity/Produto.java
+++ src/main/java/org/senai/prjjava/entity/Produto.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Produto {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
     private String nome;
