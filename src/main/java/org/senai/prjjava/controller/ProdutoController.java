73bb1273b49728be06ce5caa4c1488c18fc92758
gurgelrga <71472948+gurgelrga@users.noreply.github.com>
2022-11-13 15:46:00 -0300

    Realizando correção

--- src/main/java/org/senai/prjjava/controller/ProdutoController.java
+++ src/main/java/org/senai/prjjava/controller/ProdutoController.java
@@ -16,42 +16,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//http://localhost:8080/api/Produto/add?nome=Mariana&email=marigmmarques@gmail.com -- NESSE EXEMPLOS SÃO USADOS VARIÁVEIS DE PARÂMETRO chave=valor
 @Controller
 @RequestMapping(path = "/api/produto")
 @CrossOrigin("*")
 public class ProdutoController {
     @Autowired
-    ProdutoRepository pRepository;
-    //http://localhost:8080/api/produto/
+    private ProdutoRepository pRepository;
+
     @PostMapping("/")
-    public @ResponseBody Integer addProduto(@RequestBody Produto objP) {
-        pRepository.save(objP);
-        return objP.getId();
-      
+    public @ResponseBody Integer addProduto(@RequestBody Produto objU) {
+        pRepository.save(objU);
+        return objU.getId();
     }
 
     @GetMapping("/")
-    public @ResponseBody Iterable<Produto> listarProdutos(){
+    public @ResponseBody Iterable<Produto> buscarProdutos() {
         return pRepository.findAll();
     }
 
+    // http://localhost:8080/api/Produto/2 - NESSE EXEMPLO É USADO UMA VARIÁVEL PATH
     @GetMapping("/{id}")
-    public @ResponseBody Optional<Produto> buscarProduto(@PathVariable Integer id){
+    public @ResponseBody Optional<Produto> buscarProduto(@PathVariable Integer id) {
         return pRepository.findById(id);
-
     }
 
     @PutMapping("/")
-    public @ResponseBody Produto atualizar(@RequestBody Produto objP){
-        pRepository.save(objP);
-        return objP;
-
+    public @ResponseBody Produto atualizar(@RequestBody Produto objU) {
+        pRepository.save(objU);
+        return objU;
     }
 
     @DeleteMapping("/{id}")
-    public @ResponseBody String apagar(@PathVariable Integer id){
+    public @ResponseBody String apagar(@PathVariable Integer id) {
         pRepository.deleteById(id);
-        return "Produto apagado!";
+        return "Apagado";
     }
-    
 }
--- src/main/java/org/senai/prjjava/entity/Produto.java
+++ src/main/java/org/senai/prjjava/entity/Produto.java
@@ -5,38 +5,45 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity // This tells Hibernate to make a table out of this class
 public class Produto {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String nome;
     private String descricao;
     private Float valor;
-    
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getDescricao() {
         return descricao;
     }
+
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
     public Float getValor() {
         return valor;
     }
+
     public void setValor(Float valor) {
         this.valor = valor;
     }
+
 }
